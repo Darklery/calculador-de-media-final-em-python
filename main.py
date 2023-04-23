8a58a4847afd2a72d217b4cb1cb9e28715d99a81
Heloisa <97713511+Darklery@users.noreply.github.com>
2023-04-23 13:45:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# nota final com base nas 4 notas dos 4 bimestres faça a soma dos quatro bimestres para obter a media final
+# se a pontuaçao for igual a 6 passou
+#se menor que 6 não passou
+nota1 = float(input("Digite a nota do primeiro bimestre:"))
+nota2 = float(input("Digite a nota do segundo bimestre:"))
+nota3 = float(input("Digite a nota do terceiro bimestre:"))
+nota4 = float(input("Digite sua nota do quarto bimestre:"))
+
+media = ((nota1 + nota2 + nota3 + nota4)) / 4
+
+print(media)
